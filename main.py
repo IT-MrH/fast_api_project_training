a2dcab3ac69491c2175b2c4b7c75e2622e5fe027
Viktor Kolot <kolot87@yandex.ru>
2025-04-10 11:09:05 +0500

    create project

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from app.config import load_config
+from app.logger import logger
+
+config = load_config()
+app = FastAPI()
+
+if config.debug:
+    app.debug = True
+else:
+    app.debug = False
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
+
+
+@app.get("/custom")
+def read_custom_message(num1: int) -> dict:
+    return {"message": num1}
+
+
+@app.get("/db")
+def get_db_info():
+    logger.info(f"Connecting to database: {config.db.database_url}")
+    return {"database_url": config.db.database_url}
